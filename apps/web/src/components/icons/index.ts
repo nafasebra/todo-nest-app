7df23083f24b170040a604ce03a055f1cf489036
Nafas Ebrahimi <ebranafas@gmail.com>
2024-10-18 10:19:38 +0330

    feat: add edit functionality

--- apps/web/src/components/icons/index.ts
+++ apps/web/src/components/icons/index.ts
@@ -1,3 +1,6 @@
 export { default as AddIcon } from "./Add";
 export { default as EditIcon } from "./Edit";
 export { default as DeleteIcon } from "./Delete";
+export { default as CancelIcon } from "./Cancel";
+export { default as CheckIcon } from "./Check";
+
--- apps/web/src/components/ui/TodoItem.tsx
+++ apps/web/src/components/ui/TodoItem.tsx
@@ -1,4 +1,5 @@
-import { DeleteIcon, EditIcon } from "../icons";
+import { useRef, useState } from "react";
+import { DeleteIcon, EditIcon, CheckIcon, CancelIcon } from "../icons";
 
 interface TodoItemProps {
   item: {
@@ -10,6 +11,48 @@ interface TodoItemProps {
 }
 
 function TodoItem({ item, refetch }: TodoItemProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(item.title);
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleEdit = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const handleSave = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_SERVER_API_URL}/todo/${item.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title: editedTitle }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      console.log("Todo updated successfully");
+      setIsEditing(false);
+      refetch();
+    } catch (error) {
+      console.error("Error updating todo:", error);
+    }
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setEditedTitle(item.title);
+  };
+
   const deleteTodo = async () => {
     try {
       const response = await fetch(
@@ -31,15 +74,39 @@ function TodoItem({ item, refetch }: TodoItemProps) {
   };
 
   return (
-    <div className="flex items-center justify-between py-3 px-4 bg-neutral-800 shadow-md shadow-neutral-800">
-      <p>{item.title}</p>
-      <div className="flex items-center justify-between gap-3">
-        <button onClick={deleteTodo}>
-          <DeleteIcon />
-        </button>
-        <button>
-          <EditIcon />
-        </button>
+    <div className="flex items-center justify-between gap-3 py-3 px-4 bg-neutral-800 shadow-md shadow-neutral-800">
+      {isEditing ? (
+        <input
+          type="text"
+          ref={inputRef}
+          value={editedTitle}
+          onChange={(e) => setEditedTitle(e.target.value)}
+          className="w-full bg-transparent text-white "
+          placeholder="Edit your todo..."
+        />
+      ) : (
+        <p>{item.title}</p>
+      )}
+      <div className="flex items-center justify-between gap-4">
+        {isEditing ? (
+          <>
+            <button onClick={handleCancel}>
+              <CancelIcon />
+            </button>
+            <button onClick={handleSave}>
+              <CheckIcon />
+            </button>
+          </>
+        ) : (
+          <>
+            <button onClick={deleteTodo}>
+              <DeleteIcon />
+            </button>
+            <button onClick={handleEdit}>
+              <EditIcon />
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
